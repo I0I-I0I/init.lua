a0e8b8a756fdc8b80776a65f9a7d8075ad621070
I0I <azudjndj@gmail.com>
2025-02-15 01:16:54 +0300

    remove: some maps

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,22 +1,20 @@
 local M = { "NeogitOrg/neogit" }
 
-M.config = function()
-	require("neogit").setup({
-		commit_editor = { kind = "split" },
-		commit_select_view = { kind = "vsplit" },
-		commit_view = { kind = "vsplit" },
-		log_view = { kind = "floating" },
-		rebase_editor = { kind = "auto" },
-		reflog_view = { kind = "floating" },
-		merge_editor = { kind = "auto" },
-		description_editor = { kind = "auto" },
-		tag_editor = { kind = "auto" },
-		preview_buffer = { kind = "floating_console" },
-		popup = { kind = "split" },
-		stash = { kind = "vsplit" },
-		refs_view = { kind = "floating" },
-	})
-end
+M.opts = {
+    commit_editor = { kind = "split" },
+    commit_select_view = { kind = "vsplit" },
+    commit_view = { kind = "vsplit" },
+    log_view = { kind = "tab" },
+    rebase_editor = { kind = "auto" },
+    reflog_view = { kind = "floating" },
+    merge_editor = { kind = "auto" },
+    description_editor = { kind = "auto" },
+    tag_editor = { kind = "auto" },
+    preview_buffer = { kind = "floating_console" },
+    popup = { kind = "split" },
+    stash = { kind = "vsplit" },
+    refs_view = { kind = "floating" },
+}
 
 M.keys = {
 	{ "<leader>g", "<cmd>Neogit<cr>", desc = "Open git", { silent = true } }
