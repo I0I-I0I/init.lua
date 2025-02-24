749f43640c829ae7517a171c233adbfd3e60c2bf
I0I <azudjndj@gmail.com>
2025-02-24 13:43:00 +0300

    remove: trouble.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,7 +12,9 @@ M.keys = {
     { "<C-f><C-h>", "<cmd>Telescope help_tags theme=ivy<cr>", desc = "Find help" },
     { "<C-f><C-k>", "<cmd>Telescope keymaps theme=ivy<cr>", desc = "Find keymaps" },
     { "<C-f><C-r>", "<cmd>Telescope registers theme=ivy<cr>", desc = "Find registers" },
-    { "<C-f>\", "<cmd>Telescope man_pages theme=ivy<cr>", desc = "Find man pages" },
+    { "<C-f>m", "<cmd>Telescope man_pages theme=ivy<cr>", desc = "Find man pages" },
+    { "grr", "<cmd>Telescope lsp_references theme=ivy<cr>", desc = "Find man pages" },
+    { "grd", "<cmd>Telescope diagnostics theme=ivy<cr>", desc = "Find man pages" },
 }
 
 return M
