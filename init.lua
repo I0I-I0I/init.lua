89e00c5341750629df90fb62f1954b6d0fee3436
I0I-I0I <azudjndj@gmail.com>
2025-09-13 02:54:05 +0300

    add: multicursor.nvim and render-markdown.nvim

--- init.lua
+++ init.lua
@@ -30,8 +30,6 @@ vim.o.foldmethod = "expr"
 vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.o.langmap = "ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯЖ;ABCDEFGHIJKLMNOPQRSTUVWXYZ:,фисвуапршолдьтщзйкыегмцчняжб;abcdefghijklmnopqrstuvwxyz\\;\\,"
 
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "<C-y>", "4<C-y>")
 vim.keymap.set("n", "<C-e>", "4<C-e>")
 vim.keymap.set("n", "-", "<cmd>Fyler<cr>", { noremap = true })
@@ -41,8 +39,6 @@ vim.keymap.set("n", "<A-o>", "<cmd>tabnext<cr>", { silent = true })
 vim.keymap.set("n", "<A-i>", "<cmd>tabprev<cr>", { silent = true })
 vim.keymap.set("n", "<A-C-o>", "<cmd>tabmove +<cr>", { silent = true })
 vim.keymap.set("n", "<A-C-i>", "<cmd>tabmove -<cr>", { silent = true })
-vim.keymap.set("n", "<C-j>", "<cmd>cn<cr>", { silent = true })
-vim.keymap.set("n", "<C-k>", "<cmd>cp<cr>", { silent = true })
 vim.keymap.set("n", "<localleader><C-f>", ":e <C-r>=expand('%:p:h')<CR>/")
 vim.keymap.set("n", "<localleader><C-s>", ":sp <C-r>=expand('%:p:h')<CR>/")
 vim.keymap.set("n", "<localleader><C-v>", ":vs <C-r>=expand('%:p:h')<CR>/")
@@ -66,20 +62,9 @@ vim.cmd([[
     autocmd FileType fyler setlocal nospell ]])
 
 vim.pack.add({
-    "https://github.com/nvim-treesitter/nvim-treesitter",
     "https://github.com/ntk148v/komau.vim",
-    "https://github.com/Verf/deepwhite.nvim",
-
-    "https://github.com/wakatime/vim-wakatime",
-    "https://github.com/A7Lavinraj/fyler.nvim" })
-
-require("make")
-require("fyler").setup({confirm_simple = true, icon_provider = "none", default_explorer = true, git_status = false})
-require("nvim-treesitter.configs").setup({
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-    auto_install = true,
-    highlight = { enable = false },
-    indent = { enable = true }})
+    "https://github.com/craftzdog/solarized-osaka.nvim",
+    "https://github.com/wakatime/vim-wakatime" })
 
 -- Lazy
 vim.schedule(function()
@@ -87,27 +72,61 @@ vim.schedule(function()
     vim.o.spelllang = "en,ru"
 
     vim.api.nvim_create_user_command("LoadDapConfig", function() require("dap-config") end, {})
+    require("make")
     require("vim._extui").enable({ enable = true, msg = { target = "cmd", timeout = 4000 } })
 
     vim.pack.add({
+        "https://github.com/nvim-treesitter/nvim-treesitter",
         "https://github.com/mason-org/mason.nvim",
-        "https://github.com/artemave/workspace-diagnostics.nvim",
         "https://github.com/ibhagwan/fzf-lua",
-        "https://github.com/smjonas/live-command.nvim",
+        "https://github.com/A7Lavinraj/fyler.nvim",
+        "https://github.com/jake-stewart/multicursor.nvim",
+        "https://github.com/MeanderingProgrammer/render-markdown.nvim",
         "https://github.com/supermaven-inc/supermaven-nvim" })
 
-    require("live-command").setup({ commands = { Norm = { cmd = "norm" }, G = { cmd = "g" } }})
-    require("fzf-lua").setup({"ivy"})
+    require("fyler").setup({
+        confirm_simple = true,
+        icon_provider = "none",
+        git_status = { enabled = false },
+        icon = { directory_collapsed = "", directory_empty = "󰿟", directory_expanded = "" } })
+    require("nvim-treesitter.configs").setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true }})
     require("supermaven-nvim").setup({ ignore_filetypes = { "", "fyler" } })
+    require("render-markdown").setup({})
     require("mason").setup()
 
-    vim.cmd.abbreviate("g", "G")
-    vim.cmd.abbreviate("n", "Norm")
+    require("fzf-lua").setup({"ivy"})
     vim.keymap.set("n", "<C-p>", ":FzfLua files<cr>", { silent = true })
     vim.keymap.set("n", "", ":FzfLua grep<cr>", { silent = true })
     vim.keymap.set("n", "th", ":FzfLua helptags<cr>", { silent = true })
     vim.keymap.set("n", "tm", ":FzfLua manpages<cr>", { silent = true })
-    vim.keymap.set({ "n", "v" }, "<leader>n", ":Norm ")
+
+    local mc = require("multicursor-nvim")
+    mc.setup()
+    vim.keymap.set({"n", "x"}, "<A-k>", function() mc.lineAddCursor(-1) end)
+    vim.keymap.set({"n", "x"}, "<A-j>", function() mc.lineAddCursor(1) end)
+    vim.keymap.set({"n", "x"}, "<A-S-k>", function() mc.lineSkipCursor(-1) end)
+    vim.keymap.set({"n", "x"}, "<A-S-j>", function() mc.lineSkipCursor(1) end)
+    vim.keymap.set({"n", "x"}, "<A-n>", function() mc.matchAddCursor(1) end)
+    vim.keymap.set({"n", "x"}, "<A-S-n>", function() mc.matchSkipCursor(1) end)
+    vim.keymap.set({"n", "x"}, "<A-p>", function() mc.matchAddCursor(-1) end)
+    vim.keymap.set({"n", "x"}, "<A-S-p>", function() mc.matchSkipCursor(-1) end)
+    vim.keymap.set({"n", "x"}, "<A-a>", mc.matchAllAddCursors)
+    vim.keymap.set({"n", "x"}, "<A-q>", mc.toggleCursor)
+    vim.keymap.set("x", "I", mc.insertVisual)
+    vim.keymap.set("x", "A", mc.appendVisual)
+    vim.keymap.set("n", "<leader>gv", mc.restoreCursors)
+    mc.addKeymapLayer(function(layerSet)
+        layerSet({"n", "x"}, "<A-h>", mc.prevCursor)
+        layerSet({"n", "x"}, "<A-l>", mc.nextCursor)
+        layerSet({"n", "x"}, "<A-x>", mc.deleteCursor)
+        layerSet("n", "<leader>a", mc.alignCursors)
+        layerSet("n", "", function()
+            if not mc.cursorsEnabled() then mc.enableCursors() else mc.clearCursors() end end)
+    end)
 
     -- LSP
     vim.keymap.set("n", "grd", vim.diagnostic.setqflist, { silent = true })
@@ -127,43 +146,31 @@ vim.schedule(function()
             -- if client:supports_method("textDocument/completion") then
             --     local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
             --     client.server_capabilities.completionProvider.triggerCharacters = chars
-                vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = false })
+                vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
             -- end
-            for _, lsp in pairs({"clangd", "lua_ls", "ruff"}) do
-                if client.name == lsp then
-                    require("workspace-diagnostics").populate_workspace_diagnostics(client, args.buf)
-                end
-            end
         end })
 end)
 
 -- Color
-local diagnostic_color = "#ffffff"
+vim.cmd("colo quiet")
+local bgs = {"DiagnosticWarn", "DiagnosticError", "DiagnosticHint", "DiagnosticInfo"}
 local date = tonumber(os.date("%H"))
 if date >= 22 or date < 6 then
-    vim.pack.add({"https://github.com/xiyaowong/transparent.nvim"})
-    local groups = { "TabLineFill", "TabLineSel", "Folded", "NormalFloat" }
-    vim.g.transparent_groups = vim.list_extend(vim.g.transparent_groups or {}, groups)
-    vim.cmd("colo quiet")
     vim.o.bg = "dark"
-    vim.api.nvim_create_autocmd("VimEnter", {
-        callback = function()
-            vim.cmd.TransparentEnable()
-            vim.api.nvim_set_hl(0, "TabLine", { bg = "#707070", fg = "#000001" })
-            vim.api.nvim_set_hl(0, "TabLineSel", { fg = "#e0e2ea", bg = "#ffffff" })
-            vim.api.nvim_set_hl(0, "StatusLine", { fg = "#e0e2ea" })
-            vim.api.nvim_set_hl(0, "NormalFloat", { fg = "#dadada" })
-        end })
+    vim.pack.add({"https://github.com/xiyaowong/transparent.nvim"})
+    require("transparent").setup({
+        extra_groups = { "TabLine", "TabLineFill", "TabLineSel", "Folded", "NormalFloat" },
+        exclude_groups = { "CursorLine" } })
+    for _, bg in pairs(bgs) do vim.api.nvim_set_hl(0, bg, { fg = "#ffffff" }) end
+    vim.api.nvim_set_hl(0, "TabLine", { fg = "#6c6c6c" })
+    vim.api.nvim_set_hl(0, "TabLineSel", { fg = "#e0e2ea", bg = "#ffffff" })
+    vim.api.nvim_set_hl(0, "StatusLine", { fg = "#e0e2ea" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { fg = "#dadada" })
+    vim.api.nvim_set_hl(0, "Whitespace", { bg = "#ae0000" })
+    vim.api.nvim_set_hl(0, "CursorLine", { bg = "#121212" })
 else
-    vim.cmd.colo("deepwhite")
     vim.o.bg = "light"
-    diagnostic_color = "#000000"
+    for _, bg in pairs(bgs) do vim.api.nvim_set_hl(0, bg, { fg = "#000000" }) end
+    vim.api.nvim_set_hl(0, "NormalFloat", { fg = "NONE" })
+    vim.api.nvim_set_hl(0, "Whitespace", { bg = "#ae0000" })
 end
-vim.api.nvim_create_autocmd({ "ColorScheme", "VimEnter" }, {
-    callback = function()
-        local bgs = {"DiagnosticWarn", "DiagnosticError", "DiagnosticHint", "DiagnosticInfo"}
-        for _, bg in pairs(bgs) do vim.api.nvim_set_hl(0, bg, { fg = diagnostic_color }) end
-        vim.api.nvim_set_hl(0, "Whitespace", { bg = "#ae0000" })
-        vim.api.nvim_set_hl(0, "FylerFSDirectory", { fg = "#ae0000" })
-        vim.api.nvim_set_hl(0, "MiniPickMatchCurrent", { bg = "#707070" })
-    end })
